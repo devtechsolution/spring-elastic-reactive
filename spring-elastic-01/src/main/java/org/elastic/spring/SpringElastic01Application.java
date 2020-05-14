8404d97eb0cb6cf775158d3217242e389ad94458
devtechsolution <srivastvadevzone@gmail.com>
2020-05-15 01:47:56 +0530

    first commit

---
+++ spring-elastic-01/src/main/java/org/elastic/spring/SpringElastic01Application.java
@@ -0,0 +1,13 @@
+package org.elastic.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElastic01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElastic01Application.class, args);
+	}
+
+}
